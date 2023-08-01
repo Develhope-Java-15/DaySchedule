a431ce7f531007530b56462c60de2d47b9291ef9
Emanuele <conte.emanuele@protonmail.com>
2023-08-01 14:38:05 +0200

    implementato costruttore product e metodi addItem in GroceryList

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
     Implementare una classe GroceryList che rappresenta una lista della spesa.
     La classe, oltre al costruttore ed eventuali setter/getter, deve fornire i seguenti metodi:
@@ -5,4 +9,19 @@
     addItem(Product[]): aggiunge tutti i Product alla lista della spesa.
 */
 public class GroceryList {
+    private List<Product> groceryList;
+
+    public GroceryList () {
+        this.groceryList = new ArrayList<>();
+    }
+
+    public void addItem (Product newProduct) {
+        groceryList.add(newProduct);
+    }
+
+    public void addItem (Product[] newProducts) {
+        for (Product product : newProducts) {
+            groceryList.add(product);
+        }
+    }
 }
