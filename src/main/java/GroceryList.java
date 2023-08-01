96a7b68d4f7e8357aad851dcb1f76545c87382d4
aegroto <aegroto@protonmail.com>
2023-08-01 14:14:07 +0200

    root

---
+++ src/main/java/GroceryList.java
@@ -0,0 +1,8 @@
+/*
+    Implementare una classe GroceryList che rappresenta una lista della spesa.
+    La classe, oltre al costruttore ed eventuali setter/getter, deve fornire i seguenti metodi:
+    addItem(Product): aggiunge il Product alla lista della spesa.
+    addItem(Product[]): aggiunge tutti i Product alla lista della spesa.
+*/
+public class GroceryList {
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,7 @@
+/*
+    Implementare una classe Product avente i seguenti attributi:
+    - name: String
+    - quantity: int
+ */
+public class Product {
+}
